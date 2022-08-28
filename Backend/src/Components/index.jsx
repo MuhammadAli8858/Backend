663dcc333df2f274c78c5675deaea066606d7c08
Muhmmad ali <105370368+MuhammadAli8858@users.noreply.github.com>
2022-08-29 04:17:45 +0500

    Backend

---
+++ Backend/src/Components/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Blok } from './style'
+
+const App = () => {
+  return (
+    <Blok>
+      Hellp
+    </Blok>
+  )
+}
+
+export default App
---
+++ Backend/src/Components/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Blok = styled.div`
+color: red;
+background-color: wheat;
+width: 500px;
+height: 450px;
+`
+
+
+
