baf8ae7541f50bcc262ea680dd31d83bc9f5d4aa
Muhmmad ali <105370368+MuhammadAli8858@users.noreply.github.com>
2022-08-30 23:33:14 +0500

    Backend

--- Backend/src/Components/index.jsx
+++ Backend/src/Components/index.jsx
@@ -1,81 +1,69 @@
 import React, { useState } from "react";
-import { Blok, InputEmail, InputFirstName, InputLastName, InputPassword } from "./style";
+import {
+  Blok,
+  Button,
+  H1,
+  Input,
+  InputEmail,
+  InputFirstName,
+  InputLastName,
+  InputName,
+  InputPassword,
+} from "./style";
 
 const App = () => {
   const [state, setState] = useState({ roleIdSet: [1] });
 
   const onClick = () => {
-    fetch("https://houzing-app.herokuapp.com/api/public/auth/register",{
+    fetch("https://houzing-app.herokuapp.com/api/public/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(state),
     });
   };
 
-  const onRegister = ({ target: {name, value}  }) => {
+  const onRegister = ({ target: { name, value } }) => {
     setState({ ...state, [name]: value });
   };
+
   return (
     <Blok>
-    <h1>Sign up for a free account</h1>
-      <InputEmail
-        onChange={onRegister}
-        name="email"
-        placeholder="Email address"
-        type="text"
-      />
-      <InputLastName
-        onChange={onRegister}
-        name="firstname"
-        placeholder="First name"
-        type="text"
-      />
-      <InputFirstName
-        onChange={onRegister}
-        name="lastname"
-        placeholder="Last name"
-        type="text"
-      />
-      <InputPassword
-        onChange={onRegister}
-        name="password"
-        placeholder="Create password"
-        type="text"
-      />
-      <button onClick={onClick}>Register</button>
+      <H1>
+        <h1>Sign up for a free account</h1>
+      </H1>
+
+      <Input>
+        <InputFirstName
+          onChange={onRegister}
+          name="firstname"
+          placeholder="First name"
+          type="text"
+        />
+        <InputLastName
+          onChange={onRegister}
+          name="lastname"
+          placeholder="Last name"
+          type="text"
+        />
+      </Input>
+      <InputName>
+        <InputEmail
+          onChange={onRegister}
+          name="email"
+          placeholder="Email address"
+          type="text"
+        />
+        <InputPassword
+          onChange={onRegister}
+          name="password"
+          placeholder="Create password"
+          type="text"
+        />
+        <Button onClick={onClick}>Register</Button>
+      </InputName>
     </Blok>
   );
 };
 
 export default App;
 
-// {
-//   /* <div>
-//   <h1>{value.address}</h1>;
-//   <h3>{value.email}</h3>
-//   <h3>{value.phone}</h3>
-//   <hr/>
-// </div> */
-// }
-
-// {
-//   /* <h1>Hellp</h1> */
-// }
-// {
-//   /* {!user.length && <h1>Loading...</h1>} */
-// }
-// {
-//   /* {user.map((value) => { */
-// }
-// // return <div></div>;
-// {
-//   /* })} */
-// }
-
-// useEffect(() => {
-// setTimeout(() => {
-//   setUser(data);
-// }, 3000);
-// }, []);
-//? fetch(URL, options)  Метод JavaScripta, нужен для вызова данных с Backenda
-//? Без json() не возможно прочитать body "ReadableStream"
--- Backend/src/Components/style.js
+++ Backend/src/Components/style.js
@@ -2,18 +2,85 @@ import styled from "styled-components";
 
 export const Blok = styled.div`
   box-sizing: border-box;
-
-  position: absolute;
+  position: bottom;
   width: 500px;
   height: 400px;
   margin: 7% 31%;
   box-shadow: 0px 0px 30px 20px #dee4e2;
   background: #ffffff;
   border: 3px solid rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+`;
+export const H1 = styled.div`
+  color: #18a0fb;
+  text-indent: 15px;
+  margin-top: 64px;
+  margin-left: 15px;
+  border-color: #18a0fb;
+  font-family: "Silkscreen", cursive;
 `;
 
-export const InputEmail = styled.input``;
-export const InputLastName = styled.input``;
-export const InputFirstName = styled.input``;
-export const InputPassword = styled.input``;
+export const Input = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 43px;
+`;
+export const InputName = styled.div`
+  justify-content: center;
+  align-items: center;
+`;
 
+export const InputFirstName = styled.input`
+  padding: 5px 5px;
+  color: #18a0fb;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  width: 200px;
+  font-family: "Silkscreen", cursive;
+
+`;
+export const InputLastName = styled.input`
+  padding: 5px 5px;
+  color: #18a0fb;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  width: 200px;
+  font-family: "Silkscreen", cursive;
+
+`;
+export const InputEmail = styled.input`
+  padding: 5px 5px;
+  display: flex;
+  color: #18a0fb;
+  margin-left: 32px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  width: 430px;
+  font-family: "Silkscreen", cursive;
+
+`;
+export const InputPassword = styled.input`
+  padding: 5px 5px;
+  display: flex;
+  margin-left: 32px;
+  color: #18a0fb;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  width: 430px;
+  font-family: "Silkscreen", cursive;
+
+`;
+export const Button = styled.button`
+  background: #18a0fb;
+  border-radius: 6px;
+  padding: 11px 34px;
+  margin: 20px 10px 20px 32px;
+  border-color: #18a0fb;
+  color: white;
+  font-family: "Silkscreen", cursive;
+  :active {
+    transform: scale(0.9);
+    transition: all 0.1s linear;
+  }
+`;
