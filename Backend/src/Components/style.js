85c07b9bdd8a8001a8e0840e55857c4adf128637
Muhmmad ali <105370368+MuhammadAli8858@users.noreply.github.com>
2022-08-30 19:52:01 +0500

    Backend

--- Backend/src/Components/style.js
+++ Backend/src/Components/style.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const Blok = styled.div`
-  color: red;
-  background-color: wheat;
-  width: 450px;
+  box-sizing: border-box;
+
+  position: absolute;
+  width: 500px;
+  height: 400px;
+  margin: 7% 31%;
+  box-shadow: 0px 0px 30px 20px #dee4e2;
+  background: #ffffff;
+  border: 3px solid rgba(0, 0, 0, 0.5);
 `;
+
+export const InputEmail = styled.input``;
+export const InputLastName = styled.input``;
+export const InputFirstName = styled.input``;
+export const InputPassword = styled.input``;
+
